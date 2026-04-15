7032e5bcdf53853a4ff0ee37e49c8a582bba6e7e
berkaypicakci <berkaypicakci@outlook.com>
2026-04-15 06:42:55 +0300

    v0.1 creating project & adding auth asset & initialize auth

---
+++ Assets/_GameAssets/Scripts/Authentication/AuthenticationInit.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine.SceneManagement;
+
+public class AuthenticationInit : MonoBehaviour
+{
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        if (UnityServices.State == ServicesInitializationState.Initialized)
+        {
+            AuthenticationService.Instance.SignedIn += OnSignedIn;
+            
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+            if (AuthenticationService.Instance.IsSignedIn)
+            {
+                string userName = PlayerPrefs.GetString("UserName");
+
+                if (string.IsNullOrEmpty(userName))
+                {
+                    userName = "Player";
+                    PlayerPrefs.SetString("UserName", userName);
+                }
+
+                // _ = means we don't need to wait for the scene to load because there's no code that depends on it
+                _ = SceneManager.LoadSceneAsync(Consts.Scenes.MAIN_MENU); 
+            }
+        }
+    }
+
+    private void OnSignedIn()
+    {
+        Debug.Log("Signed in");
+        Debug.Log("User ID: " + AuthenticationService.Instance.PlayerId);
+        Debug.Log("Access Token: " + AuthenticationService.Instance.AccessToken);
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Helpers/Consts.cs
@@ -0,0 +1,7 @@
+public class Consts
+{
+    public struct Scenes
+    {
+        public const string MAIN_MENU = "MainScene";
+    }
+}
